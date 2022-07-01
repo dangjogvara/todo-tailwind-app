f9878ccabbdacf8ee2efb292dd0a54e25b34e77b
Dan Poulsen <dangjogvara@gmail.com>
2022-07-01 17:08:21 +0100

    header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = (props: { name: string }) => {
   return (
-    <div className="flex mb-3 justify-center bg-orange-500">
-      <header className="py-3 px-3 font-bold text-4xl text-white">{props.name}</header>
+    <div className="flex mb-3 justify-center bg-orange-500 rounded-2xl w-1/2 m-auto mt-5">
+      <header className=" py-3 px-3 font-bold text-4xl text-white">{props.name}</header>
     </div>
   );
 };
