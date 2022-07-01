5dd1f7d9748bacbad02a2ffeb40eab2842fada87
Dan Poulsen <dangjogvara@gmail.com>
2022-07-01 17:12:04 +0100

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from './components/Header';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="flex">
       <Header name="Todo List" />
     </div>
   );
