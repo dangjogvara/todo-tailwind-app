8fa9aa50019cdf4ca93bceace7e827bf7616fe8c
Dan Poulsen <dangjogvara@gmail.com>
2022-07-01 14:17:23 +0100

    header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
-const Header = () => {
+const Header = (props: { name: string }) => {
   return (
-    <div>
-      <header>Todo List</header>
+    <div className="flex mb-3 justify-center bg-orange-500">
+      <header className="py-3 px-3 font-bold text-4xl text-white">{props.name}</header>
     </div>
   );
 };
